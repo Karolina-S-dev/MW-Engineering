0d20e9ff3f40c4ca0af68a6485f28a16e8b8e382
Karo <karolina.szary93@gmail.com>
2026-06-05 14:10:05 +0200

    changed style for chemistry industry in projects

--- js/projects.js
+++ js/projects.js
@@ -1,35 +1,48 @@
 const video = document.querySelector(".hero-video");
 video.playbackRate = 0.6;
 
-// -----Modal Gallery Air Liquide-----
+// ----- Modal Gallery -----
 
-let btnGalleryAirLiquide = document.querySelector("#btn-gallery-air-liquide");
-let modalGalleryBackground = document.querySelector(
-  ".modal-gallery-background",
-);
+const buttons = document.querySelectorAll(".btn-activate-modal");
+const galleries = document.querySelectorAll(".modal-gallery-background");
 
-//wyświetlenie modala
-btnGalleryAirLiquide.addEventListener("click", () => {
-  modalGalleryBackground.classList.add("active");
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    galleries.forEach((gallery) => {
+      gallery.classList.remove("active");
+    });
+
+    const target = button.dataset.target;
+
+    document.getElementById(target).classList.add("active");
+  });
 });
 
+//funkcja do zamykania modala
+function closeAllGalleries() {
+  galleries.forEach((gallery) => {
+    gallery.classList.remove("active");
+  });
+}
+
 //zamykanie X
-let closingX = document.querySelector("#closing-X");
-closingX.addEventListener("click", () => {
-  modalGalleryBackground.classList.remove("active");
+const closingButtons = document.querySelectorAll("#closing-X");
+closingButtons.forEach((button) => {
+  button.addEventListener("click", closeAllGalleries);
 });
 
 //zamykanie po kliknięciu tła
-modalGalleryBackground.addEventListener("click", (e) => {
-  if (e.target === modalGalleryBackground) {
-    modalGalleryBackground.classList.remove("active");
-  }
+galleries.forEach((gallery) => {
+  gallery.addEventListener("click", (e) => {
+    if (e.target === gallery) {
+      closeAllGalleries();
+    }
+  });
 });
 
 //zamykanie na Esc
 document.addEventListener("keydown", (e) => {
   if (e.key === "Escape") {
-    modalGalleryBackground.classList.remove("active");
+    closeAllGalleries();
   }
 });
-
